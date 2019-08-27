66ee13bb7ca6610a177deadd80c3b2834e757058
andrewbastian <andrew.bastian@gmail.com>
2019-08-27 14:05:04 -0700

    Working on CSS styles

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const OperatorButton = (props) => {
   return (
-    <button>
+    <button className = "operatorBtn">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
 {props.operatorBtn.value}
     </button>
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -7,7 +7,7 @@ import { operators } from "../../../data.js";
 const Operators = (props) => {
   // STEP 2 - add the imported data to state
   return (
-    <div>
+    <div className ="ops">
      {/* STEP 3 - Use .map() to iterate over your array data and return a button
      component matching the name on the provided file. Pass
       it any props needed by the child component*/}
