efcbfd1fcbb6ad5dd73e5d52b303be2709588ecc
andrewbastian <andrew.bastian@gmail.com>
2019-08-27 13:27:32 -0700

    buttons up. some display issues to fix.

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
-
+import NumbnerButton from './NumberButton.js'
 //Import your array data to from the provided data file
-
+import {numbers} from "../../../data.js"
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [buttonNum, setButtonNum] = useState(numbers);
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+  //    {/* STEP 3 - Use .map() to iterate over your array data and return a button
+    //   component matching the name on the provided file. Pass
+    //   it any props needed by the child component*/}
+    {
+      buttonNum.map((number)=>
+     <NumbnerButton calculatorNumbers={number}/>
+  )
+    }
     </div>
   );
 };
+export default Numbers
